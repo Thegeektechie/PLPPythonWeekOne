031b98f4c21052c67d5aad3d82efa8c95d7d1753
Thegeektechie <thegeek308@gmail.com>
2024-03-06 01:06:12 +0100

    PLP Python Assignment For Week One

---
+++ user_inputs.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
